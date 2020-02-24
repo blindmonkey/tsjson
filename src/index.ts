e6d4ff9b3682979aad467ee96bb8318673df7c36
Sergey <sergey@hopper.com>
2020-02-24 18:41:56 -0500

    Export Result

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ import { AnyReader } from './readers/any-reader';
 export { Reader } from './readers/reader.interface';
 export { AbstractReader } from './readers/abstract-reader'
 export { Types } from './jstypes';
+export { Result } from './result/interface';
 
 
 export namespace TsJson {
